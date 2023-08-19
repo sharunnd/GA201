1449b56d36ea552b883017c985587665e6e08895
Your Name <sharunnd11@gmail.com>
2023-08-19 11:26:09 +0530

    done

---
+++ sprint-3/vue_intro/vue-counter/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <CounterDisplay />
+  </div>
+</template>
+
+<script>
+import CounterDisplay from './components/CounterDisplay.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CounterDisplay
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ sprint-3/vue_intro/vue-counter/src/components/CounterDisplay.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="counter">
+    <h2>Counter App</h2>
+    <div class="counter-value">{{ count }}</div>
+    <button @click="increment">Increment</button>
+    <button @click="decrement" :disabled="count === 0">Decrement</button>
+  </div>
+</template>
+<script>
+export default {
+    name: 'CounterDisplay',
+    data(){
+        return {
+            count: 0
+        }
+    },
+    methods:{
+        increment(){
+            this.count ++;
+        },
+        decrement(){
+            
+                this.count--;
+            
+        }
+    }
+}
+</script>
